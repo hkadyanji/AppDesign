77b2ada3087b0118cfd1495c4d370a75e5542024
hkadyanji <harvey013i@yahoo.com>
2015-05-07 03:14:50 -0600

    LinkDB homework

---
+++ LinkDB/linkdb_back.js
@@ -0,0 +1,108 @@
+var fs = require( "fs" );
+var http = require( "http" );
+var sqlite = require( "sqlite3" );
+
+function servFunction(req, res, state)
+{
+    var filename = req.url.substring( 1, req.url.length );
+
+    if( filename == "" )
+    {
+        filename = "./index.html";
+    }
+    if(filename == "updateContent")
+    { 
+       synContent(res, req);
+    }
+    else if ( filename.substring(0, 12) == "contentAdded")
+    {
+        updateContent( req, res , true);
+    }
+    else if( filename.substring(0, 14) == "contentRemoved")
+    {
+        updateContent( req, res , false);
+    }
+    else if( filename == "linkdb_front.js" )
+    {
+        serveFile( "linkdb_front.js", req, res );
+    }
+    else
+    {
+        serveFile( filename, req, res );
+    }
+}
+
+function serveFile( filename, req, res )
+{
+    try
+    {
+    	var html = fs.readFileSync( filename ).toString();
+    }
+    catch( e )
+    {
+    	console.log( "Error: Something bad happened trying to open " + filename );
+        res.writeHead( 404 );
+        res.end( "" );
+        return;
+    }
+
+    res.writeHead( 200 );
+    res.end( html );
+}
+
+function updateContent( req, res, state )
+{
+ var db = new sqlite.Database( "linkdb.sqlite" );
+
+ if(!req.url.split( "?" )[ 1 ]){
+
+   return;
+ }
+
+ var fresh_content = req.url.split( "?" )[ 1 ];
+ var link_inputs = fresh_content.split( "&" );
+ var link = link_inputs[0].split( "=" )[1];
+ var nickname = link_inputs[1].split( "=" )[1];
+ nickname = decodeURIComponent( ( nickname + '' ).replace( /\+/g, '%20' ) );
+ 
+if(link.substring(link.length-1, link.length) === "/"){
+ link = link.substring(0, link.length-1);
+}
+
+ if(state)
+ {
+   var sql_cmd = "INSERT INTO LINKS ('ADDRESS', 'NICKNAME') VALUES ('"+ link+"', '" + nickname + "')";
+ }
+ else
+ { 
+   if(link.match(/('localhost')/))
+{
+  link = link.substring(22, link.length);
+}
+   var sql_cmd = "DELETE FROM LINKS  WHERE ADDRESS='" + link + "' AND NICKNAME='" + nickname + "'";
+ }
+ 
+ db.run( sql_cmd );
+ db.close();
+ res.writeHead( 200 );
+ res.end( "" );
+ synContent(res, req);
+}
+
+function synContent(res, req){
+
+  var linkStream = "";
+  var db = new sqlite.Database( "linkdb.sqlite" );
+
+  db.each( "SELECT * FROM LINKS", function( err, row ) {
+   linkStream += row.ADDRESS + "&" + row.NICKNAME + "%";
+   });
+  db.close(function(){
+     res.writeHead( 200 );
+     res.end(JSON.stringify(linkStream));
+  });
+}
+
+var server = http.createServer( servFunction );
+
+server.listen( 8000 );
---
+++ LinkDB/linkdb_front.js
@@ -0,0 +1,66 @@
+function removeLink(elem)
+{ 
+
+ var toRemove = elem.parentNode.previousSibling;
+ var rLink = toRemove.childNodes[0].href;
+ if(rLink.substring(0,22) == "http://localhost:8000/")
+{
+ rLink = rLink.substring(22, rLink.length);
+}
+ var rNickname =  toRemove.childNodes[0].textContent;
+
+ var url = "contentRemoved?link="+ rLink;
+ url+= "&nickname=" + rNickname;
+
+ var tRow = elem.parentNode.parentNode;
+ tRow.remove();
+
+ var link_sender = new XMLHttpRequest();
+ link_sender.open("get", url);
+ link_sender.send();
+
+}
+
+function syncContent()
+{
+ var link_receiver = new XMLHttpRequest();
+ link_receiver.onload = linkListener;
+ link_receiver.open("get", "updateContent");
+ link_receiver.send();
+}
+
+function linkListener()
+{
+ var linkData = JSON.parse(this.responseText);
+ var linkStack = linkData.split("%");
+ var myTable = document.getElementById("myTable");
+ myTable.innerHTML = "";
+
+ for(var i = 0; i < linkStack.length -1; i++)
+{
+  var tData = linkStack[i].split("&");
+  var actData = "<tr><td><a href='"+ tData[0]+ "'>" + tData[1] +"</a></td><td><button onCLick='removeLink(this)'>Delete</button></td></tr>";
+  myTable.innerHTML += actData;
+}
+ 
+}
+
+function addContent()
+{
+ var link = document.getElementById("linkName").value;
+ var nickName = document.getElementById("nickName").value;
+
+ var url = "contentAdded?link="+ link;
+ url+= "&nickname=" + nickName;
+
+ var link_sender = new XMLHttpRequest();
+ link_sender.open("get", url);
+ link_sender.send();
+
+}
+
+function startTime(){
+ window.setInterval(syncContent, 1000);
+}
+
+
